43dcb6298f254d662474e2f71b62effa1d089270
jhonboy121 <alfredmathew05@gmail.com>
2022-01-09 14:29:01 +0530

    SystemUI: add edge light customizations [1/2]
    * Drawables and layout is from omni's commit
    * This a completely rewritten (in kt) and a non-hacky version of their work
    
    [Stallix] Adapt for A13 QPR1
    [Stallix] Adapt for A13 QPR3
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -90,5 +90,10 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.ENABLE_RIPPLE_EFFECT,
+        Settings.System.EDGE_LIGHT_ENABLED,
+        Settings.System.EDGE_LIGHT_ALWAYS_TRIGGER_ON_PULSE,
+        Settings.System.EDGE_LIGHT_REPEAT_ANIMATION,
+        Settings.System.EDGE_LIGHT_COLOR_MODE,
+        Settings.System.EDGE_LIGHT_CUSTOM_COLOR,
     };
 }
