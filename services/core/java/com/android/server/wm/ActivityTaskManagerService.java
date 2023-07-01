3462cf6256fb278fede16e8704c1237d037ae68f
ruka9909 <rr9387735@gmail.com>
2023-07-01 16:42:10 +0000

    squash drop PropImitationHooks

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -271,7 +271,6 @@ import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.usage.AppStandbyInternal;
 
-import com.android.internal.util.PropImitationHooks;
 import com.android.internal.util.arrow.cutout.CutoutFullscreenController;
 
 import java.io.BufferedReader;
@@ -1976,9 +1975,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("getFocusedRootTaskInfo()");
-        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3008,9 +3005,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!PropImitationHooks.shouldBypassTaskPermission(mContext)) {
             enforceTaskPermission("registerTaskStackListener()");
-        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
